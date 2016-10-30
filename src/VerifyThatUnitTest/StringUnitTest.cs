223660eea82ed0fe745eaf9f2c97bb6dd72a3f61
Vishal Anand <vishal_24_anand@hotmail.com>
2016-10-30 16:55:10 +0530

    Configured xUnit for unit testing and added one simple unit test to verify the configuration.

---
+++ src/VerifyThatUnitTest/StringUnitTest.cs
@@ -0,0 +1,17 @@
+﻿namespace VerifyThatUnitTest
+{
+    using Xunit;
+    using Verify;
+
+    public class StringUnitTest
+    {
+        [Theory]
+        [InlineData("hello","hello")]
+        [InlineData("Hello", "Hello")]
+        [InlineData("Vishal Anand", "Vishal Anand")]
+        public void VerifyIfStringsAreEqual(string expected, string got)
+        {
+            Verify.ThatTheyAreEqual(expected, got);
+        }
+    }
+}
